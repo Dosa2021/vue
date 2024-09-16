b78dd9e83a83d8d5c90ddc039c6341d8dfd468cd
iret-tatsuya-sugawara <tatsuya.sugawara@iret.co.jp>
2024-09-16 16:28:11 +0900

    現在時刻表示

--- nux_app/pages/index.vue
+++ nux_app/pages/index.vue
@@ -1,13 +1,29 @@
 <template>
-  <div>{{ title }}</div>
+  <section>
+  <h1>{{ title }}</h1>
+    <p>{{message}}</p>
+    <hr>
+    <pre>{{ now }}</pre>
+  </section>
 </template>
 
 <script>
 export default {
   data: function() {
     return {
-      title: 'おっぱい'
+      title: 'Hello',
+      message: 'this is message',
+      now: 'wait...'
+
     }
+  },
+  created: function() {
+    setInterval(() => {
+      var d = new Date()
+      this.now = d.getHours()
+        + ':' + d.getMinutes()
+        + ':' + d.getSeconds()
+    })
   }
 }
 </script>
