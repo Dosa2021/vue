b30de63e43ff8d721a4f69aa8c7da5db0636fda6
iret-tatsuya-sugawara <tatsuya.sugawara@iret.co.jp>
2024-09-16 19:12:00 +0900

    axiosでデータ取得

---
+++ nux_app/pages/axios.vue
@@ -0,0 +1,27 @@
+<template>
+    <section>
+        <h1>{{ title}}</h1>
+        <pre>{{ html_data }}</pre>
+    </section>
+</template>
+
+<script>
+import axios from 'axios';
+
+let url = "https://cat-fact.herokuapp.com/facts?animal_type=cat&amount=2"
+
+export default {
+    data: function() {
+        return {
+            title: 'Axios',
+            message: 'message'
+        }
+    },
+    asyncData: async function() {
+        let result = await axios.get(url)
+        console.log(result.data[0].text)
+        return { html_data: result.data[0].text }
+    }
+}
+</script>
+
