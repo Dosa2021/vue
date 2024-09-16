1a70be7916aa38afffbee01ed88872ca9fe454ca
iret-tatsuya-sugawara <tatsuya.sugawara@iret.co.jp>
2024-09-16 19:37:47 +0900

    入力したidのデータを表示

--- nux_app/pages/axios.vue
+++ nux_app/pages/axios.vue
@@ -1,26 +1,54 @@
 <template>
     <section>
         <h1>{{ title}}</h1>
-        <pre>{{ html_data }}</pre>
+        <div>
+            <input type="text" v-model="msg">
+            <button @click="doClick">Click</button>
+        </div>
+        <table>
+            <tbody>
+                <tr>
+                    <th>ID</th>
+                    <td>{{ json_data.id}} </td>
+                </tr>
+                <tr>
+                    <th>Title</th>
+                    <td>{{ json_data.title}} </td>
+                </tr>
+            </tbody>
+        </table>
     </section>
 </template>
 
 <script>
 import axios from 'axios';
 
-let url = "https://cat-fact.herokuapp.com/facts?animal_type=cat&amount=2"
+let url = "https://jsonplaceholder.typicode.com/posts/"
 
 export default {
     data: function() {
         return {
             title: 'Axios',
-            message: 'message'
+            message: 'message',
+            msg: '',
+            json_data: {}
         }
     },
-    asyncData: async function() {
-        let result = await axios.get(url)
-        console.log(result.data[0].text)
-        return { html_data: result.data[0].text }
+    // コンポーネント生成時に実行
+    // asyncData: async function() {
+    //     let id = 1
+    //     let result = await axios.get(url + id)
+    //     console.log(result.data)
+    //     console.log(result.data.id)
+    //     return { json_data: result.data }
+    // }
+    methods: {
+        doClick: function(event) {
+            axios.get(url + this.msg).then((res) => {
+                this.message = 'get ID= ' + this.msg
+                this.json_data = res.data
+            })
+        }
     }
 }
 </script>
